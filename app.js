da36bd574d17fc03cc1078fbaa7beeb90a3140ef
m_algnis <navamsingla3001@gmail.com>
2021-12-29 13:34:17 +0530

    added files and changes required for herouk

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ var app = express();
 //database connection
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://navam123:kyubhaikesahe@cluster0.kx81t.mongodb.net/local_library?retryWrites=true&w=majority';
+// Set up mongoose connection
+var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
